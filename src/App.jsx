0be648e7f6dac9e2dd1da13bb27f872f83fc81af
BarkatPrint <barkatwork25@gmail.com>
2025-05-13 22:36:02 +0530

    Added WhatsApp icon to bottom-right corner

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './Home/Footer';
 
 function App() {
   return (
-    <div>
+    <div className="relative">
       <Header />
       <Home />
       <Services />
@@ -15,18 +15,18 @@ function App() {
       <Contact />
       <Footer />
 
-      {/* WhatsApp Floating Icon */}
+      {/* WhatsApp Floating Button */}
       <a
         href="https://wa.me/917050266383"
+        className="fixed bottom-5 right-5 z-50"
         target="_blank"
         rel="noopener noreferrer"
         title="Chat with us on WhatsApp"
-        className="fixed bottom-4 right-4 z-50"
       >
         <img
           src="https://img.icons8.com/color/48/000000/whatsapp--v1.png"
           alt="WhatsApp"
-          className="w-12 h-12 hover:scale-110 transition-transform duration-300"
+          className="w-14 h-14 hover:scale-110 transition-transform duration-300 shadow-lg rounded-full"
         />
       </a>
     </div>
