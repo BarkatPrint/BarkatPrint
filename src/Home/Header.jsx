57fc3dc6ac2665beb9f984cb3da56378f7d4f25c
BarkatPrint <barkatwork25@gmail.com>
2025-05-17 01:47:17 +0530

    Update message

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -7,17 +7,7 @@ const Header = () => {
       {/* Logo */}
       <div className="text-xl font-bold">AR PRINT HUB</div>
 
-      {/* Search Box */}
-      <div className="flex items-center h-10 my-2 md:my-0">
-        <input
-          type="text"
-          placeholder="Search"
-          className="px-4 h-full rounded-l-md text-black outline-none text-sm w-40 sm:w-48 md:w-56"
-        />
-        <button className="bg-white text-black h-full px-3 rounded-r-md flex items-center justify-center">
-          <Search size={18} className="text-black" />
-        </button>
-      </div>
+     
 
       {/* Nav Links */}
       <ul className="hidden md:flex gap-4 text-xs font-light">
